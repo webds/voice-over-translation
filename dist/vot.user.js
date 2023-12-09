c37079de826bdca5202cc9c78163f629a3358fb6
Romeo <info@webds.net>
2023-12-09 23:00:48 +0200

    Update vot.user.js

--- dist/vot.user.js
+++ dist/vot.user.js
@@ -910,7 +910,7 @@ const sitesProxyTok = [
 /* harmony export */   iF: () => (/* binding */ workerHost)
 /* harmony export */ });
 // CONFIGURATION
-const workerHost = "api.browser.yandex.ru";
+const workerHost = "video.broken-pine-e184.workers.dev";
 const yandexHmacKey = "xtGCyGdTY2Jy6OMEKdTuXev3Twhkamgm";
 const yandexUserAgent =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 YaBrowser/23.7.1.1140 Yowser/2.5 Safari/537.36";
